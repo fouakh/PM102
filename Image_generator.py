2b1c1f9124ec2b221b06fd7e619d3b1e8aaaa9f2
Fouad KHELIFI <fouad001kh@gmail.com>
2024-02-07 20:27:46 +0100

    Second Commit git add .

--- Image_generator.py
+++ Image_generator.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 num_points = 5
-radius = 2
-sample_order = 5
+radius = 3
+sample_order = 7
 
 width, height = 500, 500
 
@@ -15,9 +15,9 @@ random_x = np.random.randint(0, width, num_points)
 random_y = np.random.randint(0, height, num_points)
 
 image = np.zeros((height, width))
-# for i in range(num_points):
-#     image += (X - random_x[i]) ** 2 + (Y - random_y[i]) ** 2 <= radius ** 2
-image[random_y, random_x] = 1
+for i in range(num_points):
+    image += (X - random_x[i]) ** 2 + (Y - random_y[i]) ** 2 <= radius ** 2
+# image[random_y, random_x] = 1
 
 x = np.arange(0, width, 1)
 y = np.arange(0, height, 1)
@@ -27,5 +27,5 @@ plt.imshow(image, cmap='gray')
 plt.axis("off")
 plt.show()
 
-np.savetxt(f"Diracs{num_points}-s{sample_order}.dat", image, header=f"sampling_step = 1.0e-0{sample_order}\nwavelength = 5e-07")
+np.savetxt(f"Circles{num_points}-s{sample_order}.dat", image, header=f"sampling_step = 1.0e-0{sample_order}\nwavelength = 5e-07")
 
